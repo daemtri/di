2618a6ba385d297ed8442da531b82ba47bd4d78d
duanquanyong <duanquanyong@outlook.com>
2023-03-28 23:42:15 +0800

    add comment

--- container/container.go
+++ container/container.go
@@ -9,30 +9,30 @@ var (
 	ContextKey = &struct{ name string }{name: "di.container.ContextKey"}
 )
 
-// ALL 定义了一个通配符, 用于获取所有的依赖
-// 用法: Invoke[All[MyInterface]](ctx)
+// All This container is used to get objects
+// usage: Invoke[All[MyInterface]](ctx)
 type All[T any] map[string]T
 
-// Container 定义了一个容器, 用于获取对象
+// Interface Container interface
 type Interface interface {
-	// Invoke 获取一个对象, 如果不存在, 则 panic
+	// Invoke Get a value from the map for a key, or panic if the key does not exist.
 	Invoke(ctx context.Context, typ reflect.Type) any
 }
 
-// Invoke 获取一个对象, 如果不存在, 则 panic
+// Invoke Get a value from the map for a key, or panic if none exists.
 func Invoke[T any](ctx context.Context) T {
 	return ctx.Value(ContextKey).(Interface).Invoke(ctx, reflect.TypeOf(new(T)).Elem()).(T)
 }
 
-// Simple 定义了一个简单的容器, 可用于mock测试场景
+// Simple A simple container for mocking testing scenes.
 type Simple map[reflect.Type]any
 
-// Put 添加一个对象
+// Put add an object
 func (s Simple) Put(obj any) {
 	s[reflect.TypeOf(obj)] = obj
 }
 
-// Invoke 获取一个对象, 如果不存在, 则 panic
+// Invoke Get an object or panic if it doesn't exist
 func (s Simple) Invoke(ctx context.Context, typ reflect.Type) any {
 	if v, ok := s[typ]; ok {
 		return v
